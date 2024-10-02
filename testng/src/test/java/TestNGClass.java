60ad7bc1da1137a99bc09a2d618fb3030ee104c8
arroyave <luis.arroyave@talent.com>
2024-10-02 11:56:58 -0500

    add BeforeTest & AfterTest to test file

--- testng/src/test/java/TestNGClass.java
+++ testng/src/test/java/TestNGClass.java
@@ -1,10 +1,31 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TestNGClass {
+    
+    WebDriver driver;
+    
+    @BeforeTest
+    public void setUpBrowser(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterTest
+    public void closeBrowser(){
+        driver.quit();
+    }
 
     @Test
-    public void openBrowser(){
-        System.out.println("Opening browser...");
+    public void openWebsite() throws InterruptedException{
+        System.out.println("Opening web...");
+        driver.get("https://www.google.com");
+        Thread.sleep(3000);
     }
 
     @Test
@@ -15,6 +36,11 @@ public class TestNGClass {
     @Test
     public void LogIn(){
         System.out.println("Logging in...");
+    }
+
+    @Test
+    public void AddToCart(){
+        System.out.println("Adding item to cart...");
     }
 
     @Test
